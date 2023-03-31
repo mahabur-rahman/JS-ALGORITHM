2153eadd428377a4b89c669d36220fc8119a4e87
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-03-31 22:01:30 +0600

    linear search dsa

---
+++ codeAbc/linearSearch.js
@@ -0,0 +1,11 @@
+function linearSearch(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+console.log(linearSearch([2, 3, 4, 5, 6, 7], 5));
