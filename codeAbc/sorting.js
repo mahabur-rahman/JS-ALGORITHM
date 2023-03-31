21127a80ee59ad48a6f91df5a27fd75a70472148
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-04-01 03:26:45 +0600

    bubble sort

---
+++ codeAbc/sorting.js
@@ -0,0 +1,23 @@
+// let arrName = ["dhaka", "barisal", "khulna", "bagura", "sirajgonj"];
+// let result = arrName.sort();
+// let arr = [35, 454, 51, -6, 7, 28];
+
+// let result = arr.sort((a, b) => a - b);
+// console.log(result);
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      console.log("to see", arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]; // 2
+        arr[j] = arr[j + 1]; // 2 = 53
+        arr[j + 1] = temp; // 53 = 2
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([2, 53, 23, 1, -3]));
