959c7f03fc93b7230ff86ed474e5300155a72cef
mahabur-rahman <annur4395@gmail.com>
2026-09-07 18:43:52 +0600

    Arrow function expression

---
+++ roadmap/week_1/day1.js
@@ -0,0 +1,26 @@
+// function getGrade(score) {
+//   if (score >= 80) {
+//     return "A";
+//   } else if (score >= 60) {
+//     return "B";
+//   } else if (score >= 40) {
+//     return "C";
+//   }
+
+//   return "F";
+// }
+
+// console.log(getGrade(85));
+// console.log(getGrade(65));
+// console.log(getGrade(45));
+// console.log(getGrade(30));
+
+
+// Arrow function 
+
+const result = (a, b) => {
+return a * b;
+}
+
+
+console.log(result(5, 10));
