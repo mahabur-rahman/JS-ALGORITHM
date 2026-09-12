2decebb63678f76b2bd93c32ad2c907ea71ab4bc
mahabur-rahman <annur4395@gmail.com>
2026-09-12 12:16:28 +0600

    Combination logic process not blindly work together

--- roadmap/week_1/day1.js
+++ roadmap/week_1/day1.js
@@ -1,16 +1,15 @@
-const products = [
-  { name: "Laptop", price: 1000 },
-  { name: "Phone", price: 500 },
-  { name: "Mouse", price: 50 },
+const orders = [
+  { id: 1, amount: 500, status: "completed" },
+  { id: 2, amount: 300, status: "pending" },
+  { id: 3, amount: 800, status: "completed" },
+  { id: 4, amount: 200, status: "cancelled" },
+  { id: 5, amount: 1000, status: "completed" },
 ];
 
-const discountedProducts = products.map((product) => {
-  const discountOfProduct = product.price - product.price * 0.2;
+// [500, 800, 1000] - 2300
 
-  return {
-    ...product,
-    discountPrice: discountOfProduct,
-  };
-});
+const completedAmounts = orders
+  .filter((order) => order.status === "completed")
+  .reduce((accumulator, order) => accumulator + order.amount, 0);
 
-console.log(discountedProducts);
+console.log(completedAmounts);
